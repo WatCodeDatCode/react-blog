1264935da5770a56e4e63722a2d67613f3cff587
Andrew Russell <62206738+WatCodeDatCode@users.noreply.github.com>
2020-12-07 18:57:23 +0100

    improves loader animation design

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LoadingSpinner = () => {
     return (
-        <div className="h-screen flex justify-center items-center">
+        <div className="h-90vh flex justify-center items-center">
             <div className="loader"></div>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -163,6 +163,8 @@ module.exports = {
             2: '2px',
             4: '4px',
             8: '8px',
+            16: '16px',
+            18: '18px',
         },
         boxShadow: {
             sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
