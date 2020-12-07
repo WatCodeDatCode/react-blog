c08f7d63be4d6618eef08f5da89aa4c822eff368
Andrew Russell <62206738+WatCodeDatCode@users.noreply.github.com>
2020-12-07 13:50:39 +0100

    implements MobileMenu component and additional click handling

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const MenuItems = () => {
+const MenuItems = ({ onClick }) => {
     return (
         <>
             <NavLink
@@ -9,6 +9,7 @@ const MenuItems = () => {
                 className="nav-inactive"
                 activeClassName="nav-active"
                 exact={true}
+                onClick={onClick}
             >
                 Home
             </NavLink>
@@ -17,6 +18,7 @@ const MenuItems = () => {
                 className="nav-inactive"
                 activeClassName="nav-active text-primary-500"
                 exact={true}
+                onClick={onClick}
             >
                 Blog
             </NavLink>
@@ -24,6 +26,7 @@ const MenuItems = () => {
                 to="/contact"
                 className="nav-inactive"
                 activeClassName="nav-active"
+                onClick={onClick}
             >
                 Contact
             </NavLink>
@@ -32,6 +35,7 @@ const MenuItems = () => {
                 className="nav-inactive"
                 activeClassName="nav-active"
                 exact={true}
+                onClick={onClick}
             >
                 Add blog
             </NavLink>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import MenuItems from './MenuItems'
+import MobileMenu from './MobileMenu'
 
 const Navbar = () => {
     const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false)
@@ -19,49 +20,7 @@ const Navbar = () => {
         <div className="bg-secondary-500 py-2 md:py-3">
             <div className="max-w-7xl mx-auto px-2 md:px-6 lg:px-8">
                 <div className="relative flex items-center justify-between h-16">
-                    <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
-                        <button
-                            className="inline-flex items-center justify-center p-2 rounded-md text-dark-800 hover:text-white hover:bg-dark-800 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                            aria-expanded="false"
-                            onClick={handleMobileMenuClick}
-                        >
-                            <span className="sr-only">Open main menu</span>
-                            <svg
-                                className={`${
-                                    mobileMenuIsOpen ? 'hidden' : 'block'
-                                } h-6 w-6`}
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                />
-                            </svg>
-                            <svg
-                                className={`${
-                                    mobileMenuIsOpen ? 'block' : 'hidden'
-                                } h-6 w-6`}
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
-                            </svg>
-                        </button>
-                    </div>
+                    <MobileMenu onClick={handleMobileMenuClick} />
                     <div className="flex-1 flex items-center justify-center md:items-stretch md:justify-start">
                         <a href="/" className="flex-shrink-0 flex items-center">
                             <img
@@ -147,7 +106,7 @@ const Navbar = () => {
                 className={`${mobileMenuIsOpen ? 'block' : 'hidden'} md:hidden`}
             >
                 <div className="flex flex-col px-2 pt-2 pb-3 space-y-1">
-                    <MenuItems />
+                    <MenuItems onClick={handleMobileMenuClick}/>
                 </div>
             </div>
         </div>
