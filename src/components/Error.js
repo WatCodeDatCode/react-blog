0c6a85703bff3b5415ed940145137a8ad0e098cd
Andrew Russell <62206738+WatCodeDatCode@users.noreply.github.com>
2020-12-08 16:54:15 +0100

    adds more styling to error messages

--- src/components/Error.js
+++ src/components/Error.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 const Error = ({ error }) => {
     return (
-        <div className="h-screen flex justify-center items-center">
-            <div className="text-4xl">Uh oh! It seems we have a problem:</div>
-            <p>{error}</p>
+        <div className="h-screen w-full flex flex-col justify-center content-between">
+            <div className="mx-auto">
+                <img
+                    className="h-auto"
+                    src="/images/error/gandalf-errorpage.png"
+                    alt="You shall not pass!"
+                />
+            </div>
+            <br></br>
+            <p className="text-center mx-24 font-nationalPark text-4xl m-16">
+                {error}
+            </p>
         </div>
     )
 }
