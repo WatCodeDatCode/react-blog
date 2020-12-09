0feba24cb6e77283c4ab10d7fdae808df6daefd0
Andrew Russell <62206738+WatCodeDatCode@users.noreply.github.com>
2020-12-09 19:31:17 +0100

    optimizes home css classes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,20 +5,25 @@ const Home = () => {
     return (
         <div className="relative">
             <img
-                className="hero-image object-cover opacity-80"
+                className="hero-image"
                 src="/images/hero.jpg"
                 alt="Gullible's Travels Home"
             />
             <div className="hero-overlay">
                 <div className="hero-text">
-                    <h1 className="text-primary-500 text-5xl md:text-6xl lg:text-7xl font-extrabold">Gullible's Travels <span className="text-secondary-500 text-3xl md:text-4xl font-bold block mt-6">Adventure Starts Here</span></h1>
-                    <div className="flex  mt-16 justify-between content-center text-4xl">
-                        <div className="">
-                            <NavLink className="cta-hero" to="/blog">Blog</NavLink>
-                        </div>
-                        <div className="">
-                            <NavLink className="cta-hero" to="/contact">Contact</NavLink>
-                        </div>
+                    <h1 className="hero-text-header">
+                        Gullible's Travels{' '}
+                        <span className="hero-header-span">
+                            Adventure Starts Here
+                        </span>
+                    </h1>
+                    <div className="hero-cta-container">
+                        <NavLink className="cta-hero" to="/blog">
+                            Blog
+                        </NavLink>
+                        <NavLink className="cta-hero" to="/contact">
+                            Contact
+                        </NavLink>
                     </div>
                 </div>
             </div>
