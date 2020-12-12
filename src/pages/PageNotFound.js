fa65cfa94fb4ceeffd41ee8ab7e118bad643406d
Andrew Russell <62206738+WatCodeDatCode@users.noreply.github.com>
2020-12-12 16:18:27 +0100

    fixes height of 404 page

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -3,21 +3,23 @@ import { NavLink } from 'react-router-dom'
 
 const PageNotFound = () => {
     return (
-        <div className="error-container">
-            <p className="error-header-text">404 - Page Not Found</p>
-            <div className="mx-auto">
-                <img
-                    className="error-image"
-                    src="/images/error/confused-cat.gif"
-                    alt="You shall not pass!"
-                />
+        <div className="h-90vh">
+            <div className="error-container">
+                <p className="error-header-text">404 - Page Not Found</p>
+                <div className="mx-auto">
+                    <img
+                        className="error-image"
+                        src="/images/error/confused-cat.gif"
+                        alt="You shall not pass!"
+                    />
+                </div>
+                <p className="error-text">
+                    It appears your cat walked over your keyboard.
+                </p>
+                <NavLink to="/" className="cta-button">
+                    Return home
+                </NavLink>
             </div>
-            <p className="error-text">
-                It appears your cat walked over your keyboard.
-            </p>
-            <NavLink to="/" className="cta-button">
-                Return home
-            </NavLink>
         </div>
     )
 }
